ddf2804ad90439f4c07c25d3ac33b235a5e4febc
Kevin Simper <kevin.simper@gmail.com>
2016-01-29 15:24:46 +0100

    Add better browser history

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
+import { Router, Route, IndexRoute } from 'react-router'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 import App from './components/App'
 import Games from './components/Games'
+import Details from './components/Details'
 
+const history = createBrowserHistory()
 let routes = (
-  <Router history={browserHistory}>
+  <Router history={history}>
     <Route path='/' component={App}>
       <IndexRoute component={Games}/>
+      <Route path='details' component={Details}/>
     </Route>
   </Router>
 )
