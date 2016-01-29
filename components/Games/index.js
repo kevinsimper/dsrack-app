34664e50c6d456003b0f93fc61767d8753130865
Kevin Simper <kevin.simper@gmail.com>
2016-01-29 14:44:31 +0100

    Remove title and make game a bit smaller

--- components/Games/index.js
+++ components/Games/index.js
@@ -7,7 +7,6 @@ export default class Games extends React.Component {
   render() {
     return (
       <div>
-        <h2>Games</h2>
         <div className={styles.Gameslist}>
           {data.games.map(item => <Game item={item}/>)}
         </div>
