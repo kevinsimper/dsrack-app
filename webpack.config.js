52f4c8894a5d6168d7f60eca4a23bd2164927f84
Kevin Simper <kevin.simper@gmail.com>
2016-01-29 13:26:30 +0100

    Simple react app with webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: __dirname + '/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.s?css$/,
+        loaders: ['style-loader', 'css?modules', 'autoprefixer', 'sass']
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'DS Rack',
+      filename: './index.html',
+      template: './index.html',
+      inject: true
+    })
+  ]
+}
